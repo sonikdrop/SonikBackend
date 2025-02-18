a06f9002bd1c457dc2bcd346a1f1a07021e7acf0
YoungAncient/ <tochyokoye@gmail.com>
2025-02-18 20:54:45 +0100

    Update generateToken.ts

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -2,19 +2,21 @@ import jwt from 'jsonwebtoken';
 import appConfig from '../configs/app.config';
 
 export const generateAccessToken = (userId: string, role: string): string => {
-    const access = 'auth'
-    const payload = { userId , access, role};
-    const secretKey = appConfig.JWT_SECRET;
-    const options = { expiresIn: '1d' }; // Token expires in 1 day
+    const access = 'auth';
+    const payload = { userId, access, role };
+    const secretKey: string = appConfig.JWT_SECRET || 'fallback_secret';
 
-    return jwt.sign(payload, secretKey as string, options);
+    const options: jwt.SignOptions = { expiresIn: 86400 }; // 1 day = 86400 seconds
+
+    return jwt.sign(payload, secretKey, options);
 }
 
 export const generateRefreshToken = (userId: string, role: string): string => {
-    const refresh = 'refresh'
-    const payload = { userId , refresh, role};
-    const secretKey = appConfig.JWT_SECRET;
-    const options = { expiresIn: '7d' }; // Token expires in 7 days
+    const refresh = 'refresh';
+    const payload = { userId, refresh, role };
+    const secretKey: string = appConfig.JWT_SECRET || 'fallback_secret';
 
-    return jwt.sign(payload, secretKey as string, options);
+    const options: jwt.SignOptions = { expiresIn: 604800 }; // 7 days = 604800 seconds
+
+    return jwt.sign(payload, secretKey, options);
 }
